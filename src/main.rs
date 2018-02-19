e74767506b8d608ff31eaff653fdbba3691197ff
olmanz <oliver90.manz@googlemail.com>
2018-02-19 21:18:46 +0100

    Restructured statistics module

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 extern crate mathol;
-use mathol::matrices::matrice::Matrice;
-
+use mathol::geometrics::planimetry::*;
 
 fn main() {
-    let m = Matrice::build_matrice(3, 3, vec![1, 1, -2, 1, -1, -2, 2, 3, -4]).unwrap();
-    let result = m.solve(&vec![0, 0, 0]).expect("error");
-    println!("{:?}", result);
+    let t = Triangle::build_triangle_with_edges(3, 4, 5).unwrap();
+    let (alpha, beta, gamma) = t.get_angles();
+    println!("alpha: {}, beta: {}, gamma: {}", alpha, beta, gamma);
+    let c = t.get_inner_circle();
+    let area = t.get_area();
+    let perimeter = t.get_perimeter();
 }
